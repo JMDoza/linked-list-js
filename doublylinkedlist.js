c5895f237a94e87ca091f099ab871c63513ada28
Jimmy <66657082+JMDoza@users.noreply.github.com>
2024-11-03 05:41:07 -0500

    added size, getHead, getTail, at, pop, dequeue, toString functions

--- doublylinkedlist.js
+++ doublylinkedlist.js
@@ -43,10 +43,92 @@ function doublyLinkedList() {
     head = newNode;
   };
 
+  const size = () => {
+    return listSize;
+  };
+
+  const getHead = () => {
+    return head;
+  };
+
+  const getTail = () => {
+    return tail;
+  };
+
+  const at = (index) => {
+    let pointer = 0;
+    let current = head;
+
+    while (pointer < index) {
+      current = current.nextNode;
+      pointer++;
+    }
+
+    return current;
+  };
+
+  const pop = () => {
+    temp = tail;
+    tail = temp.prevNode;
+    tail.nextNode = null;
+    temp.prevNode = null;
+    listSize--;
+
+    return temp;
+  };
+
+  const dequeue = () => {
+    temp = head;
+    head = temp.nextNode;
+    head.prevNode = null;
+    temp.nextNode = null;
+    listSize--;
+
+    return temp;
+  };
+
+  
+
+  const toString = (from) => {
+    string = "";
+    if (from === "head") {
+      current = head;
+      while (current != null) {
+        string += `${current.data} -> `;
+        current = current.nextNode;
+      }
+    } else if (from === "tail") {
+      current = tail;
+      while (current != null) {
+        string += `${current.data} -> `;
+        current = current.prevNode;
+      }
+    } else {
+      return "Invalid Direction!";
+    }
+    return string + `${current ? current.data : null}`;
+  };
+
   return {
     append,
     prepend,
+    size,
+    getHead,
+    getTail,
+    at,
+    pop,
+    dequeue,
+    toString,
   };
 }
 
 const list = doublyLinkedList();
+
+list.append("dog");
+list.append("cat");
+list.append("parrot");
+list.append("hamster");
+list.append("snake");
+list.append("turtle");
+
+console.log(list.toString("head"));
